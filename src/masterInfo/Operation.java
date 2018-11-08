12c1ac6fd0ae41f01fda51b2043ed88fa42f79b0
maimouna.dama <mayimounadama@gmail.com>
2018-11-08 12:14:35 +0100

    enumeration operation

--- src/masterInfo/Operation.java
+++ src/masterInfo/Operation.java
@@ -2,6 +2,50 @@ package masterInfo;
 
 public enum Operation {
 	
+	MULT("*") {
+		@Override
+		public double eval(double a, double b) {
+		
+			return a*b;
+		}
+	}, ADD("+") {
+		@Override
+		public double eval(double a, double b) {
+			
+			return a+b;
+		}
+	}, SUB("-") {
+		@Override
+		public double eval(double a, double b ){
+			
+			return a-b;
+		}
+	}, DIV ("/") {
+		@Override
+		public double eval(double a, double b) {
+		
+			return a/b;
+		}
+	} ;
 	
+ private  String symbole;
+ 
+ 
+  private Operation (String symbole)
+  {
+	
+	this.symbole= symbole;
+	
+}
+  public String getsymbole() {  
+      return  this.symbole ;  
+ } 
+  
+  
+  
+  
+  public abstract double eval (double a, double b);
+  
+  
   
 }
